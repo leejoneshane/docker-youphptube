b5c2cfdc515e54b8595260179dcfefb72155a90d
Sean Lee <leejoneshane@gmail.com>
2017-06-04 18:20:33 +0800

    Add files via upload

---
+++ configuration.php
@@ -0,0 +1,15 @@
+<?php
+$global['webSiteRootURL'] = 'http://localhost:8000/';
+$global['systemRootPath'] = '/var/www/localhost/htdocs/';
+
+
+$mysqlHost = '172.17.0.2';
+$mysqlUser = 'root';
+$mysqlPass = '12345678';
+$mysqlDatabase = 'youPHPTube';
+
+/**
+ * Do NOT change from here
+ */
+
+require_once $global['systemRootPath'].'objects/include_config.php';
