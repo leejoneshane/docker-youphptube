4b6ec1f66cc75c8188451e01da1a0e1fef1ee7f1
Sean Lee <leejoneshane@gmail.com>
2017-06-05 12:23:03 +0800

    Update docker-entrypoint.sh

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -5,10 +5,10 @@ if [ ! -f videos/configuration.php ]; then
   if [[ "${DOMAIN}" != "your.domain" && "${DB_HOST}" != "localhost" ]]; then
     cp /home/configuration.php /var/www/localhost/htdocs/videos/configuration.php
     sed -ri \
-        -i 's!DOMAIN!${DOMAIN}!g' \
-        -i 's!DB_HOST!${DB_HOST}!g' \
-        -i 's!DB_USER!${DB_USER}!g' \
-        -i 's!DB_PASSWORD!${DB_PASSWORD}!g' \
+        -e "s!DOMAIN!${DOMAIN}!g" \
+        -e "s!DB_HOST!${DB_HOST}!g" \
+        -e "s!DB_USER!${DB_USER}!g" \
+        -e "s!DB_PASSWORD!${DB_PASSWORD}!g" \
         "/var/www/localhost/htdocs/videos/configuration.php"
   fi
 fi
