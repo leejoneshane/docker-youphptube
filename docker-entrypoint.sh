a69947785e7c96f383b1cc4a818de281a63caf90
Sean Lee <leejoneshane@gmail.com>
2017-06-05 14:54:05 +0800

    Update docker-entrypoint.sh

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -10,8 +10,8 @@ if [ ! -f videos/configuration.php ]; then
         -e "s!DB_USER!${DB_USER}!g" \
         -e "s!DB_PASSWORD!${DB_PASSWORD}!g" \
         "/var/www/localhost/htdocs/videos/configuration.php"
-    RESULT=`mysqlshow --host=172.17.0.2 --user=root --password=12345678 | grep YouPHPTube`
-    if [ -z "${RESULT}" ]; then
+    RESULT=`mysqlshow --host=${DB_HOST} --user=${DB_USER} --password=${DB_PASSWORD} | grep YouPHPTube`
+    if [ -z "$RESULT" ]; then
       echo "create database if not exists YouPHPTube;" | mysql --host="${DB_HOST}" --user="${DB_USER}" --password="${DB_PASSWORD}"
       mysql --host="${DB_HOST}" --user="${DB_USER}" --password="${DB_PASSWORD}" YouPHPTube < /var/www/localhost/htdocs/install/database.sql
     fi
