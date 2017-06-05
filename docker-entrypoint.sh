6b0aca0cca25be92b7f5f8b29c16388628ff5d91
Sean Lee <leejoneshane@gmail.com>
2017-06-05 08:07:33 +0800

    Update docker-entrypoint.sh

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -3,11 +3,5 @@ set -e
 
 #if [ ! -f videos/configuration.php ]; then
 #fi
-    
-if [! -d /run/mysqld ]; then
-    /usr/bin/mysql_install_db --user=mysql 
-fi
 
-rm -f /run/apache2/httpd.pid
-
-/usr/bin/mysqld_safe --datadir='/var/lib/mysql' && exec httpd -DFOREGROUND
+exec httpd -DFOREGROUND
