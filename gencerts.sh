11aa17233501e2f919671f6d4d0010e692800043
Sean Lee <leejoneshane@gmail.com>
2017-06-07 11:09:22 +0800

    Update gencerts.sh

--- gencerts.sh
+++ gencerts.sh
@@ -8,12 +8,12 @@ fi
 # Initial certificate request, but skip if cached
 if [ ! -f /etc/letsencrypt/live/${DOMAIN}/fullchain.pem ]; then
   certbot certonly --webroot \
-   --webroot-path=/var/www \
+   --webroot-path=/var/www/localhost/htdocs/ \
    --domain ${DOMAIN} \
    --email "${ADMIN_EMAIL}" --agree-tos
 fi
 
-/usr/bin/sed -e "s!^(ServerName )(.*)$!\1 ${DOMAIN}!g" \
-             -e "s!^(SSLCertificateFile )(.*)$!\1 /etc/letsencrypt/live/${DOMAIN}/fullchain.pem!g" \
-             -e "s!^(SSLCertificateKeyFile )(.*)$!\1 /etc/letsencrypt/live/${DOMAIN}/privkey.pem!g" \
-             /etc/apache2/conf.d/ssl.conf
+sed -e "s!^(ServerName )(.*)$!\1 ${DOMAIN}!g" \
+    -e "s!^(SSLCertificateFile )(.*)$!\1 /etc/letsencrypt/live/${DOMAIN}/fullchain.pem!g" \
+    -e "s!^(SSLCertificateKeyFile )(.*)$!\1 /etc/letsencrypt/live/${DOMAIN}/privkey.pem!g" \
+    /etc/apache2/conf.d/ssl.conf
