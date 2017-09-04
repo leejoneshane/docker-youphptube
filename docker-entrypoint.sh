0fc4ccc6904a1ae7987ec1691a5bdd21ed0aa09d
Sean Lee <leejoneshane@gmail.com>
2017-09-04 12:33:54 +0800

    Update docker-entrypoint.sh

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -19,7 +19,7 @@ if [[ "${DOMAIN}" != "your.domain" && "${DB_HOST}" != "localhost" ]]; then
     mysql --host="${DB_HOST}" --user="${DB_USER}" --password="${DB_PASSWORD}" youPHPTube < /var/www/localhost/htdocs/install/database.sql
     echo "USE youPHPTube; INSERT INTO users (id, user, password, created, modified, isAdmin) VALUES (1, 'admin', md5('${ADMIN_PASSWORD}'), now(), now(), true);" | mysql --host="${DB_HOST}" --user="${DB_USER}" --password="${DB_PASSWORD}"
     echo "USE youPHPTube; INSERT INTO categories (id, name, clean_name, created, modified) VALUES (1, 'Default', 'default', now(), now());" | mysql --host="${DB_HOST}" --user="${DB_USER}" --password="${DB_PASSWORD}"
-    echo "USE youPHPTube; INSERT INTO configurations (id, video_resolution, users_id, version, webSiteTitle, language, contactEmail,  created, modified) VALUES (1, '858:480', 1,'4.0', '${SITE_TITLE}', 'tw', '${ADMIN_EMAIL}', now(), now());" | mysql --host="${DB_HOST}" --user="${DB_USER}" --password="${DB_PASSWORD}"
+    echo "USE youPHPTube; INSERT INTO configurations (id, video_resolution, users_id, version, webSiteTitle, language, contactEmail,  created, modified, encoderURL) VALUES (1, '858:480', 1,'4.0', '${SITE_TITLE}', 'tw', '${ADMIN_EMAIL}', now(), now(), 'https://${DOMAIN}/encoder');" | mysql --host="${DB_HOST}" --user="${DB_USER}" --password="${DB_PASSWORD}"
   fi
 
   if mysqlshow --host=${DB_HOST} --user=${DB_USER} --password=${DB_PASSWORD} youPHPTube-Encoder; then
@@ -27,8 +27,8 @@ if [[ "${DOMAIN}" != "your.domain" && "${DB_HOST}" != "localhost" ]]; then
   else
     echo "CREATE DATABASE youPHPTube-Encoder CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;" | mysql --host="${DB_HOST}" --user="${DB_USER}" --password="${DB_PASSWORD}"
     mysql --host="${DB_HOST}" --user="${DB_USER}" --password="${DB_PASSWORD}" youPHPTube-Encoder < /var/www/localhost/htdocs/encoder/install/database.sql
-    echo "USE youPHPTube-Encoder; INSERT INTO streamers (id, siteURL, user, pass, priority, isAdmin, created, modified) VALUES (1, '${DOMAIN}', 'admin', md5('${ADMIN_PASSWORD}'), now(), now());" | mysql --host="${DB_HOST}" --user="${DB_USER}" --password="${DB_PASSWORD}"
-    echo "USE youPHPTube-Encoder; INSERT INTO configurations (id, allowedStreamerURL, defaultPriority, created, modified, version) VALUES (1, '', 1, now(), now(), '4.0');" | mysql --host="${DB_HOST}" --user="${DB_USER}" --password="${DB_PASSWORD}"
+    echo "USE youPHPTube-Encoder; INSERT INTO streamers (id, siteURL, user, pass, priority, isAdmin, created, modified) VALUES (1, 'https://${DOMAIN}', 'admin', md5('${ADMIN_PASSWORD}'), 1, 1, now(), now());" | mysql --host="${DB_HOST}" --user="${DB_USER}" --password="${DB_PASSWORD}"
+    echo "USE youPHPTube-Encoder; INSERT INTO configurations (id, allowedStreamerURL, defaultPriority, created, modified, version) VALUES (1, 'https://${DOMAIN}', 1, now(), now(), '1.0');" | mysql --host="${DB_HOST}" --user="${DB_USER}" --password="${DB_PASSWORD}"
   fi
 fi
 
