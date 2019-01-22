2f6eeba1592fe9502562354a7677f2e333610d13
Sean Lee <leejoneshane@gmail.com>
2019-01-22 10:29:10 +0800

    Update configuration.php

--- configuration.php
+++ configuration.php
@@ -1,8 +1,12 @@
 <?php
+$global['disableAdvancedConfigurations'] = 0;
+$global['videoStorageLimitMinutes'] = 0;
 $global['webSiteRootURL'] = 'PROTOCOL://DOMAIN/';
 $global['systemRootPath'] = '/var/www/localhost/htdocs/';
+$global['salt'] = 'SALT';
 
 $mysqlHost = 'DB_HOST';
+$mysqlPort = '3306';
 $mysqlUser = 'DB_USER';
 $mysqlPass = 'DB_PASSWORD';
 $mysqlDatabase = 'youPHPTube';
