b116988def1c42ff9a8b2962a1ba0a2a3189691c
Sean Lee <leejoneshane@gmail.com>
2019-04-01 12:17:16 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -21,7 +21,7 @@ if [[ "${DOMAIN}" != "your.domain" && "${DB_HOST}" != "localhost" ]]; then
     mysql --host="${DB_HOST}" --user="${DB_USER}" --password="${DB_PASSWORD}" youPHPTube < /var/www/localhost/htdocs/install/database.sql
     echo "USE youPHPTube; INSERT INTO users (id, user, email, password, created, modified, isAdmin) VALUES (1, 'admin', '${ADMIN_EMAIL}', md5('${ADMIN_PASSWORD}'), now(), now(), true);" | mysql --host="${DB_HOST}" --user="${DB_USER}" --password="${DB_PASSWORD}"
     echo "USE youPHPTube; INSERT INTO categories (id, name, clean_name, description, created, modified) VALUES (1, 'Default', 'default', '', now(), now());" | mysql --host="${DB_HOST}" --user="${DB_USER}" --password="${DB_PASSWORD}"
-    echo "USE youPHPTube; INSERT INTO configurations (id, video_resolution, users_id, version, webSiteTitle, language, contactEmail, encoderURL, created, modified) VALUES (1, '858:480', 1, '4.0', '${SITE_TITLE}', '${LANG}', '${ADMIN_EMAIL}', 'https://${DOMAIN}/encoder', now(), now());" | mysql --host="${DB_HOST}" --user="${DB_USER}" --password="${DB_PASSWORD}"
+    echo "USE youPHPTube; INSERT INTO configurations (id, video_resolution, users_id, version, webSiteTitle, language, contactEmail, encoderURL, created, modified) VALUES (1, '858:480', 1, '6.9', '${SITE_TITLE}', '${LANG}', '${ADMIN_EMAIL}', 'https://${DOMAIN}/encoder', now(), now());" | mysql --host="${DB_HOST}" --user="${DB_USER}" --password="${DB_PASSWORD}"
   fi
 fi
 
