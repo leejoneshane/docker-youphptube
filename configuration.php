caee3f814d4fb4176d5cfe6b1c2cc1ffbed742ec
Sean Lee <leejoneshane@gmail.com>
2020-11-25 08:46:05 +0800

    Update configuration.php

--- configuration.php
+++ configuration.php
@@ -1,9 +1,16 @@
 <?php
+$global['configurationVersion'] = 2;
 $global['disableAdvancedConfigurations'] = 0;
 $global['videoStorageLimitMinutes'] = 0;
 $global['webSiteRootURL'] = 'PROTOCOL://DOMAIN/';
 $global['systemRootPath'] = '/var/www/localhost/htdocs/';
 $global['salt'] = 'SALT';
+$global['disableTimeFix'] = 0;
+$global['enableDDOSprotection'] = 1;
+$global['ddosMaxConnections'] = 40;
+$global['ddosSecondTimeout'] = 5;
+$global['strictDDOSprotection'] = 0;
+$global['noDebug'] = 0;
 
 $mysqlHost = 'DB_HOST';
 $mysqlPort = '3306';
